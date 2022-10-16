a8c2251073e80fcd0a6b7d445331734193942251
Udvita25 <81299808+Udvita25@users.noreply.github.com>
2022-10-16 20:57:52 +0530

    Add files via upload

---
+++ easy.js
@@ -0,0 +1,14 @@
+function setup(){
+    canvas=createCanvas(600, 600);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+    function prelod(){
+
+    }
+
+    function draw(){
+        image(video,0,0,600,600);
+    }
